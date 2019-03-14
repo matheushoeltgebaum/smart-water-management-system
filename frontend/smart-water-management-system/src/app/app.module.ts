993a3d7b31bc6703e379a4a87105747254320ffb
matheushoeltgebaum <pereiramateduardo@gmail.com>
2019-03-14 20:18:24 -0300

    Tela de login

--- frontend/smart-water-management-system/src/app/app.module.ts
+++ frontend/smart-water-management-system/src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [
+  declarations:
+  [
     AppComponent,
     LoginComponent
   ],
-  imports: [
+  imports:
+  [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/smart-water-management-system/src/app/login/login.component.ts
+++ frontend/smart-water-management-system/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    let username = this.loginForm.value.username;
+    let password = this.loginForm.value.password;
+
+    localStorage.setItem('userId', '5c882843e833d936baff9d47');
+    localStorage.setItem('password', 'a8dae2c3ad1119923f9742eda3a525d4');
   }
 
 }
