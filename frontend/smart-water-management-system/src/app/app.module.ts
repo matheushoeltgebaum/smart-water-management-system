8bf1e7bc6d6e4e20c2b36a92da141be357d5fe4a
Matheus Eduardo Pereira <matheus.pereira@facilcorp.local>
2019-03-15 13:04:03 -0300

    Alterações dashboard

--- frontend/smart-water-management-system/src/app/app.module.ts
+++ frontend/smart-water-management-system/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ChartComponent } from './chart/chart.component';
+import { ChartComponent } from './shared/chart/chart.component';
 
 @NgModule({
   declarations:
